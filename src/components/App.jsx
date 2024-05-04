f4761d0df7706603af9c7aba09db7e49b3ec44ec
Al14lex <alenahomenko1421@gmail.com>
2024-05-04 10:26:58 +0200

    hw435

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,11 +33,11 @@ export default function App() {
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} />
+      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback}/>
       {totalFeedback === 0 && <Notification />}
       {totalFeedback > 0 && (
         <>
-          <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
         </>
       )}
     </>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,15 @@
 
-export default function Options({ updateFeedback, resetFeedback }) {
+export default function Options({ updateFeedback, resetFeedback, totalFeedback}) {
   const handleClick = feedbackType => {
     updateFeedback(feedbackType);
   };
- 
+  
   return (
     <div>
       <button onClick={() => handleClick('good')}>Good</button>
       <button onClick={() => handleClick('neutral')}>Neutral</button>
       <button onClick={() => handleClick('bad')}>Bad</button>
-      <button onClick={resetFeedback}>Reset</button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
     </div>
   );
 }
